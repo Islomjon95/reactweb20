d3e283283e0bdaae486cf700cbfa0b6c7cee1361
islomjon95 <inogomovislom@gmail.com>
2026-04-08 17:40:55 +0500

    Commit qo'shamiz

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,24 +4,24 @@ function Navbar() {
   return (
     <div className='nav'>
       <div className="logo">
-        <a href="#">
+        <a href="https://example.com">
             <img src="./img/Logo.png" alt="" />
         </a>
       </div>
       <ul className='right_menu'>
         <li className="menu_item">
-            <a href="#">
+            <a href="https://example.com">
                 <img src="./img/person.png" alt="" />
                 Log in
             </a>
         </li>
         <li className="menu_item">
-            <a href="#">
+            <a href="https://example.com">
                 <img src="./img/cart.png" alt="" />0
             </a>
         </li>
         <li className="menu_item">
-            <a href="#">
+            <a href="https://example.com">
                 <img src="./img/chiziq.png" alt="" />
             </a>
         </li>
